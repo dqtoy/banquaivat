a81f0685a7f6ddda92e5c741a3cb46301898d4d6
Thien Nguyen Duc <20168806@student.hust.edu.vn>
2019-11-14 16:35:18 +0700

    up code cho social platform sua

---
+++ Assets/Scripts/PlayerScripts/PlayerDiChuyen.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDiChuyen : MonoBehaviour
+{
+
+    private CharacterController character_Controller;
+
+    private Vector3 move_Direction;
+
+    public float speed = 5f;
+    public float gravity = 1f;
+    public float acceleration = 1f;
+    public float jump_Force = 10f;
+
+    private float vertical_Velocity;
+
+    void Awake()
+    {
+        character_Controller = GetComponent<CharacterController>();
+        Physics.gravity = new Vector3(0, 1f, 0);
+    }
+
+    void Update()
+    {
+        MoveThePlayer();
+    }
+
+    void MoveThePlayer()
+    {
+
+        move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f,
+                                     Input.GetAxis(Axis.VERTICAL));
+
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+
+        ApplyAcceleration();
+
+        character_Controller.Move(move_Direction);
+
+
+    } // move player
+
+    void ApplyAcceleration()
+    {
+
+        vertical_Velocity -= acceleration * Time.deltaTime;
+
+        // jump
+        PlayerJump();
+
+        move_Direction.y = vertical_Velocity * Time.deltaTime;
+
+    } // apply acceleration
+
+    void PlayerJump()
+    {
+
+        if (character_Controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            vertical_Velocity = jump_Force;
+        }
+
+    }
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
